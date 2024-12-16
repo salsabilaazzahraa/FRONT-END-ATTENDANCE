c85570da6b6758c8f92ea5527011362cb27de57c
SALSABILA AZZAHRA <156277867+salsabilaazzahraa@users.noreply.github.com>
2024-12-17 05:38:37 +0700

    Update information_screen.dart

--- aurora_presence_flutter/lib/screens/information_screen.dart
+++ aurora_presence_flutter/lib/screens/information_screen.dart
@@ -1,6 +1,5 @@
 import 'package:aurora_presence_flutter/screens/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class InformationScreen extends StatefulWidget {
   @override
@@ -9,12 +8,12 @@ class InformationScreen extends StatefulWidget {
 
 class _InformationScreenState extends State<InformationScreen> {
   // Default values for pengumuman and faq
-  List<String> pengumuman = [
+  final List<String> pengumuman = [
     'Kantor tutup pada tanggal 17 Agustus untuk Hari Kemerdekaan.',
     'Workshop internal tentang keamanan siber akan diadakan pada 1 September.',
   ];
   
-  List<Map<String, String>> faq = [
+  final List<Map<String, String>> faq = [
     {'question': 'Bagaimana cara reset password email kantor?', 'answer': 'Silakan hubungi tim IT di it.support@idsteknologi.co.id.'},
     {'question': 'Apa saja fasilitas yang disediakan di kantor?', 'answer': 'Kami menyediakan kantin, ruang olahraga, dan ruang kesehatan.'},
     {'question': 'Bagaimana cara mengajukan cuti?', 'answer': 'Pengajuan cuti bisa dilakukan melalui portal HR di intranet perusahaan.'},
@@ -29,210 +28,12 @@ class _InformationScreenState extends State<InformationScreen> {
   ];
 
   @override
-  void initState() {
-    super.initState();
-    _loadData();
-  }
-
-  Future<void> _savePengumuman() async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setStringList('pengumuman', pengumuman);
-  }
-
-  Future<void> _loadPengumuman() async {
-    final prefs = await SharedPreferences.getInstance();
-    final List<String>? loadedPengumuman = prefs.getStringList('pengumuman');
-    if (loadedPengumuman != null) {
-      setState(() {
-        pengumuman = loadedPengumuman;
-      });
-    }
-  }
-
-  Future<void> _saveFaq() async {
-    final prefs = await SharedPreferences.getInstance();
-    final String faqString = faq.map((e) => '${e['question']}|${e['answer']}').join(';');
-    await prefs.setString('faq', faqString);
-  }
-
-  Future<void> _loadFaq() async {
-    final prefs = await SharedPreferences.getInstance();
-    final String? faqString = prefs.getString('faq');
-    if (faqString != null) {
-      setState(() {
-        faq = faqString.split(';').map((item) {
-          final parts = item.split('|');
-          return {
-            'question': parts[0],
-            'answer': parts[1],
-          };
-        }).toList();
-      });
-    }
-  }
-
-  Future<void> _loadData() async {
-    await _loadPengumuman();
-    await _loadFaq();
-  }
-
-  void _editPengumuman(int index) {
-    TextEditingController controller = TextEditingController(text: pengumuman[index]);
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Edit Pengumuman'),
-          content: TextField(
-            controller: controller,
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  pengumuman[index] = controller.text;
-                });
-                _savePengumuman(); // Save changes
-                Navigator.pop(context);
-              },
-              child: Text('Simpan'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void _addPengumuman() {
-    TextEditingController controller = TextEditingController();
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Tambah Pengumuman'),
-          content: TextField(
-            controller: controller,
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  pengumuman.add(controller.text);
-                });
-                _savePengumuman(); // Save changes
-                Navigator.pop(context);
-              },
-              child: Text('Tambah'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void _removePengumuman(int index) {
-    setState(() {
-      pengumuman.removeAt(index);
-    });
-    _savePengumuman(); // Save changes
-  }
-
-  void _editFaq(int index) {
-    TextEditingController questionController = TextEditingController(text: faq[index]['question']);
-    TextEditingController answerController = TextEditingController(text: faq[index]['answer']);
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Edit FAQ'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: questionController,
-                decoration: InputDecoration(labelText: 'Pertanyaan'),
-              ),
-              TextField(
-                controller: answerController,
-                decoration: InputDecoration(labelText: 'Jawaban'),
-              ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  faq[index] = {
-                    'question': questionController.text,
-                    'answer': answerController.text,
-                  };
-                });
-                _saveFaq(); // Save changes
-                Navigator.pop(context);
-              },
-              child: Text('Simpan'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void _addFaq() {
-    TextEditingController questionController = TextEditingController();
-    TextEditingController answerController = TextEditingController();
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Tambah FAQ'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: questionController,
-                decoration: InputDecoration(labelText: 'Pertanyaan'),
-              ),
-              TextField(
-                controller: answerController,
-                decoration: InputDecoration(labelText: 'Jawaban'),
-              ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  faq.add({
-                    'question': questionController.text,
-                    'answer': answerController.text,
-                  });
-                });
-                _saveFaq(); // Save changes
-                Navigator.pop(context);
-              },
-              child: Text('Tambah'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void _removeFaq(int index) {
-    setState(() {
-      faq.removeAt(index);
-    });
-    _saveFaq(); // Save changes
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-  backgroundColor: Color(0xFF00CEE8),
-  leading: IconButton(
-    icon: Icon(Icons.arrow_back_ios, color: Colors.white),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF00CEE8),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios, color: Colors.white),
           onPressed: () {
             Navigator.pushReplacement(
               context,
@@ -240,15 +41,14 @@ class _InformationScreenState extends State<InformationScreen> {
             );
           },
         ),
-  title: Text(
-    "Informasi",
-    style: TextStyle(
-      color: Colors.white,
-      fontWeight: FontWeight.bold,
-    ),
-  ),
-),
-
+        title: Text(
+          "Informasi",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: ListView(
@@ -259,31 +59,12 @@ class _InformationScreenState extends State<InformationScreen> {
               context,
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: pengumuman.asMap().entries.map((entry) {
-                  int index = entry.key;
-                  String item = entry.value;
+                children: pengumuman.map((item) {
                   return ListTile(
                     title: Text(item),
-                    trailing: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        IconButton(
-                          icon: Icon(Icons.edit),
-                          onPressed: () => _editPengumuman(index),
-                        ),
-                        IconButton(
-                          icon: Icon(Icons.delete),
-                          onPressed: () => _removePengumuman(index),
-                        ),
-                      ],
-                    ),
                   );
                 }).toList(),
               ),
-            ),
-            TextButton(
-              onPressed: _addPengumuman,
-              child: Text('Tambah Pengumuman'),
             ),
             SizedBox(height: 16),
 
@@ -301,34 +82,16 @@ class _InformationScreenState extends State<InformationScreen> {
               context,
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: faq.asMap().entries.map((entry) {
-                  int index = entry.key;
-                  Map<String, String> item = entry.value;
+                children: faq.map((item) {
                   return ListTile(
                     title: Text('Q: ${item['question']}'),
                     subtitle: Text('A: ${item['answer']}'),
-                    trailing: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        IconButton(
-                          icon: Icon(Icons.edit),
-                          onPressed: () => _editFaq(index),
-                        ),
-                        IconButton(
-                          icon: Icon(Icons.delete),
-                          onPressed: () => _removeFaq(index),
-                        ),
-                      ],
-                    ),
                   );
                 }).toList(),
               ),
             ),
-            TextButton(
-              onPressed: _addFaq,
-              child: Text('Tambah FAQ'),
-            ),
             SizedBox(height: 16),
+
             _buildSectionHeader('Hubungi Kami', Icons.contact_phone, context),
             SizedBox(height: 8),
             _buildBoxedContent(
@@ -373,7 +136,7 @@ class _InformationScreenState extends State<InformationScreen> {
         border: Border.all(color: Colors.blue),
       ),
       padding: EdgeInsets.all(12),
-      child:child,
-);
-}
+      child: child,
+    );
+  }
 }
